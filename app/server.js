6a700510ba3179777a1bea95b43728ef720481c9
Atul Kamble <atul_kamble@hotmail.com>
2025-11-13 08:30:58 +0530

    update

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Kubernetes!\n');
+  res.end('Hello from project!\n');
 });
 
 server.listen(port, () => {
